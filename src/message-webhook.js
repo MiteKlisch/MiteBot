84bec530f395d7cd98307009cfdcb44dc4d4ae57
Volodymyr <klischvolodimir@gmail.com>
2020-01-03 21:46:18 +0200

    nothing

--- src/message-webhook.js
+++ src/message-webhook.js
@@ -8,18 +8,14 @@ const messageWebhook = (req, res) => {
         
         body.entry.forEach((entry) => {
             let webhook_event = entry.messaging[0];
-            //console.log(webhook_event);
 
             let sender_psid = webhook_event.sender.id;
-            //console.log(`Sender PSID: ${sender_psid}`);
             
             if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
-                //console.log(webhook_event.message)
-                
+
             } else if (webhook_event.postback) {
                handlePostback(sender_psid, webhook_event.postback)
-               //console.log(webhook_event.postback)
             }
         });
 
