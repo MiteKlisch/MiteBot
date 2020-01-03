17b9f91009d954d8098b3fe9c0d64c0e31571f00
Volodymyr <klischvolodimir@gmail.com>
2020-01-03 21:46:37 +0200

    added links with music for chatbot

---
+++ src/multimedia/video.js
@@ -0,0 +1,29 @@
+module.exports = {
+    rock: [
+        'https://www.youtube.com/watch?v=Rbm6GXllBiw',
+        'https://www.youtube.com/watch?v=hTWKbfoikeg',
+        'https://www.youtube.com/watch?v=SDea7laHD4E',
+        'https://www.youtube.com/watch?v=ZhIsAZO5gl0',
+        'https://www.youtube.com/watch?v=sZfZ8uWaOFI'
+    ],
+
+    pop: [
+        'https://www.youtube.com/watch?v=Zi_XLOBDo_Y',
+        'https://www.youtube.com/watch?v=DyDfgMOUjCI',
+        'https://www.youtube.com/watch?v=dv13gl0a-FA',
+        'https://www.youtube.com/watch?v=G510jeWiaV0'
+    ],
+
+    rap: [
+       'https://www.youtube.com/watch?v=MeB3eYk1Ze0',
+       'https://www.youtube.com/watch?v=R2WxaeIJcqY',
+       'https://www.youtube.com/watch?v=87w1y874gwM'
+    ],
+
+    classical: [
+        'https://www.youtube.com/watch?v=wygy721nzRc',
+        'https://www.youtube.com/watch?v=Rb0UmrCXxVA',
+        'https://www.youtube.com/watch?v=6JQm5aSjX6g',
+        'https://www.youtube.com/watch?v=4i0TnNI6U-w'
+    ]
+}
