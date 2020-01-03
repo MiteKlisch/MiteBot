47657d9e9fb90c39747f313b50016ccef825b203
Volodymyr <klischvolodimir@gmail.com>
2020-01-03 21:45:58 +0200

    added simple GET 'Helo world'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const verifyWebhook = require('./verify-webhook');
 const messageWebhook = require('./message-webhook');
-
+const request = require('request');
 
 
 const app = express();
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
 
 
 app.get('/webhook', verifyWebhook);
