fab895796d25888f45e8346737bef3bb34af344e
Volodymyr <klischvolodimir@gmail.com>
2020-01-03 21:45:27 +0200

    added getStartedbutton and links with music

--- src/functions.js
+++ src/functions.js
@@ -1,59 +1,131 @@
 const request = require('request');
+const video = require('./multimedia/video')
+
+
+
+const askTemplate = (text) => {
+  return {
+      "attachment":{
+          "type":"template",
+          "payload":{
+              "template_type":"button",
+              "text": text,
+              "buttons":[
+                  {
+                      "type":"postback",
+                      "title":"Rock",
+                      "payload":"ROCK"
+                  },
+                  {
+                      "type":"postback",
+                      "title":"Pop",
+                      "payload":"POP"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Classic",
+                    "payload":"CLASSIC"
+                  }
+              ]
+          }
+      }
+  }
+};
+
+
 
 const handleMessage = (sender_psid, received_message) => {
-    let response;
+  let response;
 
-    if (received_message.text) {
-        response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
-          }
-    } else if(received_message.attachments) {
-        // Gets the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
+  if (received_message.text) {
+      response = {
+          "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+     // response = askTemplate();
+  } 
 
-        response = {
-            "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "generic",
-                "elements": [{
-                  "title": "Is this the right picture?",
-                  "subtitle": "Tap a button to answer.",
-                  "image_url": attachment_url,
-                  "buttons": [
-                    {
-                      "type": "postback",
-                      "title": "Yes!",
-                      "payload": "yes",
-                    },
-                    {
-                      "type": "postback",
-                      "title": "No!",
-                      "payload": "no",
-                    }
-                  ],
-                }]
-              }
-            }
+  callSendAPI(sender_psid, response); 
+}
+
+ const videoTemplate = (type, sender_id) => {
+  return {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"Your music :)!",
+        "buttons":[
+          {
+            "type":"web_url",
+            "url": getVideo(type, sender_id),
+            "title":"Link",
+            "webview_height_ratio": "full"
           }
+        ]
+      }
     }
+  }
+ };
 
-    callSendAPI(sender_psid, response);
-
+let users = {};
+ 
+const getVideo = (type, sender_id) => {
+    // если записи о пользователе пока нет - создадим её
+    if(users[sender_id] === undefined){
+        users = Object.assign({
+            [sender_id] : {
+                'rock_count' : 0,
+                'pop_count' : 0,
+                'classic_count' : 0
+            }
+        }, users);
+    }
+ 
+    let count = video[type].length, // общее количество изображений нужного типа
+        user = users[sender_id], // пользователь, ожидающий ответа
+        user_type_count = user[type+'_count'];
+ 
+ 
+    // обновим сведения о пользователе до отправки ответа
+    let updated_user = {
+        [sender_id] : Object.assign(user, {
+            [type+'_count'] : count === user_type_count + 1 ? 0 : user_type_count + 1
+        })
+    };
+    // обновим список пользователей
+    users = Object.assign(users, updated_user);
+ 
+    console.log(users);
+    return video[type][user_type_count];
 }
 
 const handlePostback = (sender_psid, recivedPostback) => {
     let response;
     const payload = recivedPostback.payload
 
-    if (payload === 'yes') {
-        response = { "text": "Thanks!"}
-    } else if (payload === 'no') {
-        response = { "text": "OOPS!"}
+    if (payload === 'ROCK') {
+      response = videoTemplate('rock', sender_psid);
+      callSendAPI(sender_psid, response, function(){
+          callSendAPI(sender_psid, askTemplate('Show me more'));
+      });
+    } else if (payload === 'POP') {
+      response = videoTemplate('pop', sender_psid);
+      callSendAPI(sender_psid, response, function(){
+          callSendAPI(sender_psid, askTemplate('Show me more'));
+      });
+    } else if (payload === 'CLASSIC') {
+      response = videoTemplate('classical', sender_psid);
+      callSendAPI(sender_psid, response, function(){
+          callSendAPI(sender_psid, askTemplate('Show me more'));
+      });
     }
-
-    callSendAPI(sender_psid, response);
+    else if (payload === 'GET_STARTED') {
+        response = askTemplate('What music do you want to listen to?');
+        callSendAPI(sender_psid, response);
+    }
+    
 }
+
 
 const callSendAPI = (sender_psid, response) => {
     // Construct the message body
@@ -78,6 +150,7 @@ const callSendAPI = (sender_psid, response) => {
     }
   }); 
 }
+
 
 module.exports = {
     handleMessage,
